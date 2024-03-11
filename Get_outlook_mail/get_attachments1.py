3a7c735cf298ea1c71397ab25cabf1ceff5a8518
evgeniy <pavlinbl4@gmail.com>
2024-03-11 20:31:13 +0300

    add os.path.abspath(__file__) to files

--- Get_outlook_mail/get_attachments1.py
+++ Get_outlook_mail/get_attachments1.py
@@ -1,13 +1,16 @@
 import os.path
-from exchangelib import FileAttachment, ItemAttachment, Message
+from exchangelib import FileAttachment
 from colorama import Fore
 from read_outlook import authorization, make_soup
 from pathlib import Path
 from loguru import logger
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+logger.add(f'{script_dir}/log.log', rotation='10:00')
+
 
 def messages_history() -> list:
-    with open('received_messages.txt', 'r') as messages_history_file:
+    with open(f'{script_dir}/received_messages.txt', 'r') as messages_history_file:
         received_messages_id = messages_history_file.readlines()
     return [x.strip() for x in received_messages_id]
 
@@ -19,6 +22,7 @@ def download_attachments():
     # received_messages_id = []
 
     for item in account.inbox.all().order_by('-datetime_received')[:5]:
+        logger.info(f'{item.sender.email_address = }')
 
         if item.message_id not in received_messages_id:
 
@@ -32,16 +36,14 @@ def download_attachments():
                 print(Fore.WHITE + make_soup(letter))
                 for attachment in item.attachments:
                     if isinstance(attachment, FileAttachment) and attachment.size > 9000:
-                        logger.info(attachment.name)
                         local_path = os.path.join(f"{Path.home()}/Downloads/Ъ downloads", attachment.name)
                         with open(local_path, "wb") as f:
                             f.write(attachment.content)
-                        print("Saved attachment to", local_path)
-                    elif isinstance(attachment, ItemAttachment):
-                        if isinstance(attachment.item, Message):
-                            print(attachment.item.subject, attachment.item.body)
-                with open('received_messages.txt', 'a') as messages_history_file:
+                        logger.info(f"Saved attachment to {local_path}")
+                with open(f'{script_dir}/received_messages.txt', 'a') as messages_history_file:
                     messages_history_file.write(f'{item.message_id}\n')
+        else:
+            logger.info('No new messages')
 
 
 if __name__ == '__main__':
